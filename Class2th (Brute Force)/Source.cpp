9367cb2afed5f0944d909312d8522e0a9d094b8b
godkz1234 <ddddeong@naver.com>
2024-12-12 21:43:04 +0900

    (2024.12.12) 거품 정렬

---
+++ Class2th (Brute Force)/Source.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#pragma region 브루트 포스
+	// 가능한 모든 경우의 수를 탐색하면서 
+	// 결과를 도출하는 알고리즘입니다.
+
+	int password[3] = { 9,4,5 };
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			for (int k = 0; k < 10; k++)
+			{
+				if (password[0] == i && password[1] == j && password[2] == k)
+				{
+					cout << "자물쇠 해제" << endl;
+					cout << "비밀번호는 : " <<
+						password[0] << password[1] << password[2] << endl;
+
+				}
+			}
+		}
+	}
+
+#pragma endregion
+}
---
+++ Class3th (Bubble Sort)/Source.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define SIZE 5
+
+int main()
+{
+#pragma region 거품 정렬
+	// 서로 인접한  두 원소를 비교하여 정렬하는 알고리즘입니다.
+
+	int arr[SIZE] = { 5,1,4,7,2 };
+
+	cout << "정렬 전 : ";
+	for (int i = 0; i < 5; i++) 
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+
+	for (int i = 0; i < SIZE - 1; i++)
+	{
+		for (int j = 0; j < (SIZE - i) - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				std::swap(arr[j], arr[j + 1]);
+			}
+		}
+	}
+	
+	for (const int & element : arr)
+	{
+		cout << element << " ";
+	}
+
+#pragma endregion
+
+	return 0;
+}
+
