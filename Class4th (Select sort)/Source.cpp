00bd1bdf16d08b78b5dec74c77eb72e36c4f1fa9
godkz1234 <ddddeong@naver.com>
2024-12-13 21:43:52 +0900

    (2024.12.13) 선택 정렬

---
+++ Class4th (Select sort)/Source.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+#define SIZE 5
+
+int main()
+{
+#pragma region 선택 정렬
+	// 주어진 리스트 중에 최소값을 찾아서 맨 앞에 위치한
+	// 결과를 교체하는 방식으로 정렬하는 알고리즘입니다.
+
+	int list[SIZE] = { 9, 2, 13, 1 ,5 };
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		int min = list[i];
+
+		int select = i;
+
+		for (int j = i + 1; j < SIZE; j++)
+		{
+			if (min > list[j])
+			{
+				min = list[j];
+				select = j;
+			}
+		}
+
+		std::swap(list[i], list[select]);
+	}
+
+	cout << "결과 : ";
+
+	for (const int& element : list)
+	{
+		cout << element << " ";
+	}
+#pragma endregion
+
+
+	return 0;
+}
+
