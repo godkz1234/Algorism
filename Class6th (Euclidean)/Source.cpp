f46a444633339af71cd83a5821170834a564c7aa
godkz1234 <ddddeong@naver.com>
2024-12-16 21:44:50 +0900

    (2024.12.16) 투 포인터

---
+++ Class6th (Euclidean)/Source.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int Euclid(int x, int y)
+{
+	// 1. 두 개의 자연수 x, y가 있습니다.
+	//    그리고 x와 y로 나눈 나머지를 R이라고 합니다.
+
+	// 2. R이 0이라면, x와 y의 최대공약수 y가 됩니다.
+
+	// 2. R이 0아니라면, x와 y의 최대공약수는 y와 r의
+	//	  최대공약수와 같기 때문에 x에는 y 그리고 y에는 R
+	//	  를 대입한 후 1번으로 돌아갑니다.
+
+	if (y == 0)
+	{
+		return x;
+	}
+	else
+	{
+		return Euclid(y, x % y);
+	}
+
+}
+
+int main()
+{
+#pragma region 유클리드 호제법
+	// 2개의 자연수 또는 정식의 최대 공약수를 구하는 방식으로
+	// 두 수가 서로 상대방 수를 나누어서 원하는 수를 얻어내는 알고리즘입니다.
+
+	// int x = 0;
+	// int y = 0;
+	// 
+	// int result = 0;
+	// 
+	// cin >> x >> y;
+	// 
+	// for (int i = 1; i <= x && i <= y; i++)
+	// {
+	// 	if (x % i == 0 && y % i == 0)
+	// 	{
+	// 		result = i;
+	// 	}
+	// }
+	// 
+	// cout << "최대 공약수 : " << result << endl;
+
+	cout << Euclid(695, 1112) << endl;
+
+#pragma endregion
+
+
+	return 0;
+}
+
---
+++ Class7th (Two Pointer)/Source.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define SIZE 5
+
+int main()
+{
+#pragma region 투 포인터
+	// 두 개의 포인터를 활용해서 배열의 요소를 효과적으로
+	// 탐색하는 알고리즘입니다.
+
+	// 1. 시작점과 끝나는 지점이 첫번째 원소를 가리키도록 합니다.
+
+	// 2. 현재 부분합이 m과 같다면 count를 증가시킵니다.
+
+	// 3. 현재 부분합이 m보다 작다면 end를 1 증가시킵니다.
+
+	// 4. 현재 부분합이 m보다 크거나 같다면 start를 1 증가시킵니다.
+
+	int count = 0;
+	int sum = 0; // 부분합
+
+	int start = 0; // 시작점
+	int end = 0; // 끝나는 지점
+
+	int m = 5; // 연속적인 수열의 개수
+
+	int list[SIZE] = { 1,2,3,2,5 };
+
+	while (start <= end)
+	{
+		if (sum >= m) // 4번 조건
+		{
+			sum -= list[start++];
+		}
+		else if (end > SIZE) // 버퍼 오버플로우를 방지하기 위해 SIZE를 넘어갔을때 강제 종료
+		{
+			break;
+		}
+		else // 3번 조건
+		{
+			sum += list[end++];
+		}
+		if (sum == m) // 2번 조건
+		{
+			count++;
+		}
+	}
+
+	cout << "count의 값 : " << count << endl; // 출력
+
+#pragma endregion
+
+
+	return 0;
+}
+
